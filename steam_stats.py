1fefc3f746069c510cf90e446e5fd840d83db114
Karthik Guruswamy <karthik.guruswamy@h2o.ai>
2021-11-22 06:50:00 -0800

    Added timeline filter

--- steam_stats.py
+++ steam_stats.py
@@ -5,7 +5,7 @@
 #
 # Author: Karthik Guruswamy, karthik.guruswamy@h2o.ai
 #
-# Last updated: Oct 26, 2021
+# Last updated: Nov 22, 2021
 #
 
 import os
@@ -25,7 +25,6 @@ import re
 
 @app('/')
 async def serve(q: Q):
-    print(q.args)
 
     if not q.client.initialized:
             initialize_app_for_new_client(q)
@@ -39,6 +38,9 @@ async def serve(q: Q):
     if q.args.back_button:
             await render_first_page(q)
             
+    if q.args.show_timeline:
+            await set_timeline(q)
+
     await q.page.save()
 
 async def handle_uploaded_data(q: Q):
@@ -46,8 +48,6 @@ async def handle_uploaded_data(q: Q):
     if q.page['back_button']:
             del q.page['back_button']
             del q.page['daily_peak_sessions_users']
-##        del q.page['instance_usage']
-##        del q.page['gpu_usage']
             del q.page['product_usage']
             del q.page['user_usage']
     
@@ -55,6 +55,7 @@ async def handle_uploaded_data(q: Q):
 
     # Download new dataset to data directory
     q.client.working_file_path = await q.site.download(url=q.args.file_upload[0], path=data_path)
+    q.client.timeline = 'All'
 
     # Update views to end user
     render_table_summary_info(q)
@@ -64,11 +65,10 @@ async def handle_uploaded_data(q: Q):
 async def render_first_page(q:Q):
     del q.page['back_button']
     del q.page['daily_peak_sessions_users']
-##    del q.page['instance_usage']
-##    del q.page['gpu_usage']
     del q.page['product_usage']
     del q.page['user_usage']
     render_table_summary_info(q)
+    q.client.state = "render_first_page"
     
 async def drill_down_charts(q:Q):
     """Deletes tables and summary cards and renders nice charts"""
@@ -76,11 +76,20 @@ async def drill_down_charts(q:Q):
     del q.page['peak_usage']
     del q.page['summary_view']
     del q.page['drill_button']
-
+    del q.page['timeline']
 
     back_button = [ui.button(name='back_button', label='<< Back', primary=True)]
     q.page['back_button'] = ui.form_card(box='1 8 2 1',items=back_button)
+    q.client.state = "render_charts"
     render_charts(q)
+
+async def set_timeline(q:Q):
+    q.client.timeline = q.args.choice_group
+
+    if (q.client.state == "render_charts"):
+        await drill_down_charts(q)
+    else:
+        await render_first_page(q)
 
 def initialize_app_for_new_client(q):
     """Setup this Wave application for each browser tab by creating a page layout and setting any needed variables"""
@@ -106,6 +115,7 @@ def initialize_app_for_new_client(q):
             os.mkdir(q.client.data_path)
 
     q.client.initialized = True
+    q.client.timeline = 'All'
 
 
 def render_upload_view(q: Q):
@@ -125,36 +135,57 @@ def render_table_summary_info(q: Q):
     if (q.page['back_button']):
             del q.page['back_button']
             del q.page['daily_peak_sessions_users']
-##        del q.page['instance_usage']
-##        del q.page['gpu_usage']
             del q.page['product_usage']
             del q.page['user_usage']
 
     # Raw data view
-    items = [ui.separator(label='Raw Dataset')]
+    labelx = "Raw Dataset. Viewing Date Range : "+q.client.timeline
+    items = [ui.separator(label=labelx)]
     items.append(ui.text_xl(os.path.basename(q.client.working_file_path)))
-    items.append(make_ui_table(file_path=q.client.working_file_path,n_rows=10000, name='head_of_table'))
+    items.append(make_ui_table(file_path=q.client.working_file_path,n_rows=10000, name='head_of_table', timeline=q.client.timeline))
     q.page['table'] = ui.form_card(box='3 2 9 6', items=items)
 
     # Peak Usage view
     peak_usage = [ui.separator(label='Peak Usage by Day')]
-    peak_usage.append(make_ui_processed(file_path=q.client.working_file_path,name='peak_by_day_stats'))
+    peak_usage.append(make_ui_processed(file_path=q.client.working_file_path,name='peak_by_day_stats', timeline=q.client.timeline))
     q.page['peak_usage'] = ui.form_card(box='6 8 6 -1',items=peak_usage)
 
     # Summary view
     summary = [ui.separator(label='Summary')]
-    summary.append(make_ui_summary(file_path=q.client.working_file_path,name='summary'))
+    summary.append(make_ui_summary(file_path=q.client.working_file_path,name='summary', timeline=q.client.timeline))
     q.page['summary_view'] = ui.form_card(box='3 8 3 -1',items=summary)
 
     # Setup the 'Drill Down' button
     drill_button = [ui.button(name='drill_button', label='Drill Down  >>', primary=True)]
     q.page['drill_button'] = ui.form_card(box='1 8 2 1',items=drill_button)
 
+    # Setup the Timeline Choices
 
-def make_ui_table(file_path: str, n_rows: int, name: str):
+    clabel = 'Date Range -> '+q.client.timeline
+    q.page['timeline'] = ui.form_card(box = '1 9 2 5', items=[
+        ui.choice_group(name='choice_group',label=clabel,value=q.client.timeline,
+                        required=True,
+                        choices=[
+                            ui.choice('All','All'),
+                            ui.choice('First 30 days','First 30 days'),
+                            ui.choice('Recent 30 days','Recent 30 days'),
+                            ui.choice('Recent 60 days','Recent 60 days'),
+                            ui.choice('Recent month','Recent month'),
+                            ui.choice('Prev month','Prev month'),
+                            ui.choice('Recent year','Recent year'),
+                            ui.choice('Prev year','Prev year')
+                        ]
+                        ),
+        ui.button(name='show_timeline',label='Set Date Range',primary=True),
+    ])
+
+
+def make_ui_table(file_path: str, n_rows: int, name: str, timeline: str):
     """Creates a ui.table object from a csv file"""
 
     df = pd.read_csv(file_path)
+    df = filter_rows_by_timeline(df, timeline)
+
     n_rows = min(n_rows, df.shape[0])
     
 
@@ -167,9 +198,10 @@ def make_ui_table(file_path: str, n_rows: int, name: str):
     )
     return table
 
-def make_ui_summary(file_path: str, name: str):
+def make_ui_summary(file_path: str, name: str, timeline: str):
 
     sessions = pd.read_csv(file_path)
+    sessions = filter_rows_by_timeline(sessions, timeline)
 
     #
     # no. of rows and cols
@@ -183,21 +215,21 @@ def make_ui_summary(file_path: str, name: str):
     # Hours
     #
 
-    q = "select sum(session_duration_sec)/3600 as hours from sessions"
-    hd = psql.sqldf(q,locals())
+    qq = "select sum(session_duration_sec)/3600 as hours from sessions"
+    hd = psql.sqldf(qq,locals())
     hours = round(hd['hours'].values[0],2)
     
     #
     # Min and Max timestamps
     #
-    q = """
+    qq = """
                     select min(datetime(session_launch_unix,'unixepoch')) as min_ts, 
                                max(datetime(session_end_unix,'unixepoch')) as max_ts 
                     from sessions
                     where session_state='finished'
             """
 
-    min_max_ts = psql.sqldf(q, locals())
+    min_max_ts = psql.sqldf(qq, locals())
     
     min_ts = min_max_ts['min_ts'].values[0]  
     max_ts = min_max_ts['max_ts'].values[0]
@@ -209,14 +241,14 @@ def make_ui_summary(file_path: str, name: str):
     # Aggregate stats on users and products
     #
     if 'username' in sessions.columns:
-                    q = """
+                    qq = """
                                     select count(distinct username) as unique_users, 
                                                count(distinct version) as unique_versions
                                     from sessions
                                     where session_state='finished'
                             """
 
-                    agg_users_products = psql.sqldf(q, locals())
+                    agg_users_products = psql.sqldf(qq, locals())
                     users = agg_users_products['unique_users'].values[0]
                     versions = agg_users_products['unique_versions'].values[0]
     else:
@@ -228,7 +260,7 @@ def make_ui_summary(file_path: str, name: str):
     #
     peak_usage = pd.read_csv("data/peak_usage.csv")
 
-    q = """
+    qq = """
                     select max(unique_users) as max_daily_peak,
                                avg(unique_users) as avg_daily_peak,
                                max(peak_cpus) as max_daily_cpus,
@@ -239,7 +271,7 @@ def make_ui_summary(file_path: str, name: str):
                                sum(case when unique_users = 0 then 1 else 0 end) as total_days_not_used
                             from peak_usage
             """
-    agg_peak = psql.sqldf(q,locals())
+    agg_peak = psql.sqldf(qq,locals())
     max_daily_peak = agg_peak['max_daily_peak'].values[0]
     avg_daily_peak = round(agg_peak['avg_daily_peak'].values[0],2)
     max_daily_cpus = agg_peak['max_daily_cpus'].values[0]
@@ -284,21 +316,23 @@ def make_ui_summary(file_path: str, name: str):
     return table
 
 
-def make_ui_processed(file_path: str, name: str):
+def make_ui_processed(file_path: str, name: str, timeline: str):
 
     sessions = pd.read_csv(file_path)
-    
+
+    sessions = filter_rows_by_timeline(sessions, timeline)
+
     #
     # Min and Max timestamps
     #
-    q = """
+    qq = """
                     select min(datetime(session_launch_unix,'unixepoch')) as min_ts, 
                                max(datetime(session_end_unix,'unixepoch')) as max_ts 
                     from sessions
                     where session_state='finished'
             """
 
-    min_max_ts = psql.sqldf(q, locals())
+    min_max_ts = psql.sqldf(qq, locals())
     min_ts = min_max_ts['min_ts'].values[0]
     max_ts = min_max_ts['max_ts'].values[0]
 
@@ -306,14 +340,14 @@ def make_ui_processed(file_path: str, name: str):
     # Aggregate stats on users and products
     #
     if 'ownerN' in sessions.columns:
-            q = """
+            qq = """
                             select count(distinct username) as unique_users, 
                                        count(distinct version) as unique_versions 
                             from sessions
                             where session_state='finished'
                     """
 
-            agg_users_products = psql.sqldf(q, locals())
+            agg_users_products = psql.sqldf(qq, locals())
             users = agg_users_products['unique_users'].values[0]
             versions = agg_users_products['unique_versions'].values[0]
     else:
@@ -323,26 +357,26 @@ def make_ui_processed(file_path: str, name: str):
     #
     # Steps to get to peak users and other peak aggs
     #
-    q = """
+    qq = """
                     select *, datetime(session_launch_unix,'unixepoch') as start_ts, 
                                       datetime(session_end_unix,'unixepoch') as end_ts 
                     from sessions
                     where session_state='finished'
             """
-    v_basetable = psql.sqldf(q,locals())
+    v_basetable = psql.sqldf(qq,locals())
 
-    q = """
+    qq = """
                     select date(min_ts) as minval,
                                date(max_ts) as maxval
                             from min_max_ts
             """
-    min_max = psql.sqldf(q,locals())
+    min_max = psql.sqldf(qq,locals())
 
     rx = pd.date_range(start=min_max['minval'].values[0], end=min_max['maxval'].values[0], freq='D')
     range_values = pd.DataFrame(rx)
     range_values.columns = ['day_present']
 
-    q = """
+    qq = """
                     select *
                             from
                             (select * from v_basetable) a
@@ -352,9 +386,9 @@ def make_ui_processed(file_path: str, name: str):
                                     and
                                      date(a.end_ts) >= date(b.day_present))
             """
-    base_table_expanded = psql.sqldf(q,locals())
+    base_table_expanded = psql.sqldf(qq,locals())
 
-    q = """
+    qq = """
                     select date(a.day_present) as 'Day Present',
                             sum(case when b.start_ts is null then 0 else 1 end) as 'Peak Sessions',
                             sum(case when b.cpu_count is null then 0 else b.cpu_count end) as 'Peak CPUs',
@@ -368,7 +402,7 @@ def make_ui_processed(file_path: str, name: str):
                     group by 1
                     order by 1
             """
-    peak_usage = psql.sqldf(q,locals())
+    peak_usage = psql.sqldf(qq,locals())
     peak_usage.to_csv("data/peak_usage.csv",header=['day_present','peak_sessions','peak_cpus','peak_gpus','unique_users'])
 
     #
@@ -391,6 +425,8 @@ def make_ui_processed(file_path: str, name: str):
 
 def render_charts(q:Q):
     sessions = pd.read_csv(q.client.working_file_path)
+    sessions = filter_rows_by_timeline(sessions, q.client.timeline)
+
     peak_usage = pd.read_csv('data/peak_usage.csv')
     #header=['day_present','peak_sessions','peak_cus','unique_users'])               )
     
@@ -406,7 +442,7 @@ def render_charts(q:Q):
     
     
     q.page['daily_peak_sessions_users'] = ui.vega_card(
-                                                    box='3 2 8 4',
+                                                    box='3 2 9 6',
                                                     title='Daily Peak Sessions and Unique Users',
                                                     specification=spec,
                                               )
@@ -421,7 +457,7 @@ def render_charts(q:Q):
     product_usage = psql.sqldf(qq,locals())
 
     q.page['product_usage'] = ui.plot_card(
-                        box='3 6 4 4',
+                        box='3 8 3 -1',
                         title='Version Usage by Sessions',
                         data=data(
                             fields=product_usage.columns.tolist(),
@@ -461,7 +497,7 @@ def render_charts(q:Q):
 
     
     q.page['user_usage'] = ui.vega_card(
-                                box='7 6 4 4',
+                                box='6 8 6 -1',
                                 title='Top 15 Power Users by Sessions/Hours',
                                 specification=spec,
                             )
@@ -504,3 +540,75 @@ def altair_area_line_chart(data: pd, x: str, x_title: str, y1:str, y1_title:str,
     spec = alt.layer(area_A, line_B).resolve_scale(y='independent').to_json()
     return spec   
      
+def filter_rows_by_timeline(df: pd, x_filter: str):
+ 
+#     ui.choice('All','All'),
+#     ui.choice('First 30 days','First 30 days'),
+#     ui.choice('Recent 30 days','Recent 30 days'),
+#     ui.choice('Recent 60 days','Recent 60 days'),
+#     ui.choice('Recent month','Recent month'),
+#     ui.choice('Prev month','Prev month'),
+#     ui.choice('Recent year','Recent year'),
+#     ui.choice('Prev year','Prev year')
+ 
+
+   # Create markers for filtering
+   qq = """ select 
+             max(date(session_launch_date)) as latest_day, 
+              min(date(session_launch_date,'+30 days')) as first_30_days,
+              max(date(session_launch_date, 'start of year')) as latest_year_start,
+              max(date(session_launch_date, 'start of year','-1 year')) as prev_year_start,
+                            
+              max(date(session_launch_date, 'start of month')) as latest_month_start,
+              max(date(session_launch_date, 'start of month','-1 month')) as prev_month_start,
+              
+              max(strftime('%Y', session_launch_date)) as current_year,
+              max(strftime('%W', session_launch_date)) as current_week,
+              max(date(session_launch_date,'-30 days')) as recent_30_days,
+              max(date(session_launch_date,'-60 days')) as recent_60_days
+
+         from df
+      """
+
+   markers = psql.sqldf(qq,locals()) 
+
+   # Fallback
+   df2 = df
+
+   if (x_filter == 'First 30 days'):
+       # First 30 days
+       qq = "select a.* from df a join markers b where a.session_launch_date <=  first_30_days"
+       df2 = psql.sqldf(qq,locals())
+
+   if (x_filter == 'Recent 30 days'):
+       # Recent 30 days
+       qq = "select a.* from df a join markers b where a.session_launch_date >=  recent_30_days"
+       df2 = psql.sqldf(qq,locals())
+
+   if (x_filter == 'Recent 60 days'):
+       # Recent 60 days
+       qq = "select a.* from df a join markers b where a.session_launch_date >=  recent_60_days"
+       df2 = psql.sqldf(qq,locals())
+
+   if (x_filter == 'Recent month'):
+       # Recent month
+       qq = "select a.* from df a join markers b where a.session_launch_date >= latest_month_start"
+       df2 = psql.sqldf(qq,locals())
+
+   if (x_filter == 'Prev month'):
+       # Prev month
+       qq = "select a.* from df a join markers b where a.session_launch_date >= b.prev_month_start and a.session_launch_date < b.latest_month_start"
+       df2 = psql.sqldf(qq,locals())
+
+   if (x_filter == 'Recent year'):
+       # Recent year
+       qq = "select a.* from df a join markers b where a.session_launch_date >= b.latest_year_start"
+       df2 = psql.sqldf(qq,locals())
+
+   if (x_filter == 'Prev year'):
+       # Prev year
+       qq = "select a.* from df a join markers b where a.session_launch_date >= b.prev_year_start and a.session_launch_date < b.latest_year_start"
+       df2 = psql.sqldf(qq,locals())
+
+   return df2
+
